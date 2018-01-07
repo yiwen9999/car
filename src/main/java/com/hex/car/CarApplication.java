e967242b9d1a07225ca0204273309a1ba5905f11
yiwen9999 <yiwen9999@live.cn>
2018-01-07 14:31:03 +0800

    mac 2018-01-07 14:30

--- src/main/java/com/hex/car/CarApplication.java
+++ src/main/java/com/hex/car/CarApplication.java
@@ -2,11 +2,24 @@ package com.hex.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarApplication.class, args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.setSessionTimeout(864000);//单位为S
+            }
+        };
     }
 }
